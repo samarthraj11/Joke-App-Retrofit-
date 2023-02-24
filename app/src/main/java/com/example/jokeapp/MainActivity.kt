ab7de4fd79b7e45fc5d8a28784fc697af3de1bfc
Samarth Raj <mailforsamarth@gmail.com>
2023-02-24 15:46:12 +0530

    retrofit check

--- app/src/main/java/com/example/jokeapp/MainActivity.kt
+++ app/src/main/java/com/example/jokeapp/MainActivity.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.lifecycleScope
 import com.example.jokeapp.ui.theme.JokeAppTheme
 import retrofit2.HttpException
@@ -20,30 +24,44 @@ class MainActivity : ComponentActivity() {
             JokeAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
                 ) {
-//                    var joke = "It is yet to come"
-//                    lifecycleScope.launchWhenCreated {
-//                        val response = try {
-//                            RetrofitInstance.api.getJoke()
-//                        } catch (
-//                            e: IOException
-//                        ) {
-//                            Log.e("something", "No internet")
-//                            return@launchWhenCreated
-//
-//                        } catch (e: HttpException) {
-//                            Log.e("something", "request error")
-//                            return@launchWhenCreated
-//                        }
-//                        if(response.isSuccessful && response.body() != null) {
-//                            joke = response.body()!!.setup
-//                        }
-//
-//                    }
-                    JokeHome()
+                    var joke = "nothing"
+                    lifecycleScope.launchWhenCreated {
+                        val response = try {
+                            RetrofitInstance.api.getJoke()
+                        } catch (
+                            e: IOException
+                        ) {
+                            Log.e("something", "No internet")
+                            return@launchWhenCreated
+                        } catch (e: HttpException) {
+                            Log.e("something", "request error")
+                            return@launchWhenCreated
+                        }
+                        if (response.isSuccessful && response.body() != null) {
+                            joke = response.body()!!.setup
+                        }
+                        Log.d("output", joke)
+                    }
+                    Column(
+                        modifier = Modifier
+                            .padding(15.dp)
+                            .fillMaxHeight()
+                            .fillMaxWidth(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(text = joke)
+                    }
                 }
             }
         }
     }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
 }
